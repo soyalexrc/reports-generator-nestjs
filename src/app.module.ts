9d0a84e0c9b12d0bafbe8262d1779dfbeaacbc98
alex rodriguez <alexcarvajal2404@gmail.com>
2024-11-14 22:01:54 -0500

    :zap: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BasicReportsModule } from './basic-reports/basic-reports.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BasicReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BasicReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
